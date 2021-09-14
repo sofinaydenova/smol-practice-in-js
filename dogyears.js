1cafa7e686e12385cd15de5e77fd468a04f7a5fc
__________sofi____________(✧ω✧) <53216481+sofinaydenova@users.noreply.github.com>
2021-09-14 22:47:58 +0300

    2 js proj
    if I were a dog 
    U・ᴥ・U

---
+++ dogyears.js
@@ -0,0 +1,17 @@
+// what would my age be if I were a dog
+
+var myAge = 26 // i'm so old :(
+// first 2 years as a dog count for 10.5 human years
+let earlyYears = 2
+earlyYears = 2 * 10.5
+// the following years count as 4
+var laterYears = myAge - 2
+laterYears = laterYears * 4
+console.log(earlyYears)
+console.log(laterYears)
+
+myAgeInDogYears = earlyYears + laterYears
+
+myName = 'Sofi'.toLowerCase()
+
+console.log(`My name is ${myName}. I am HUMAN AGE years old in human years which is ${myAgeInDogYears} years old in dog years.`)
